e57ea6e048c813623bd00e3ac1d01c1e337c8f80
Kajal <kajalpoojari555@gmail.com>
2024-10-10 22:02:22 +0530

    hompage react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+   <>
+   <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+            <h1 className='text-light'>Stock Prediction Portal</h1>
+            <p className='text-light lead'> ry6u n7r8 46jn5 756hg 54y66fdrhyy 466tgy456tgg 46y57fdxnh r gvb  httyhrs gvyyt thty
+                fdhiuen ri rer fertgif
+                hfuieytnbtgui t5uyerne4 t
+                tgitu95 pp jopio780hfjrb3438y099ewuiwgefh hyur4hnbeyr.
+            </p>
+            <Button text='Login' class='btn-info'/>
+        </div>
+   </div>
+   </>
+  )
+}
+
+export default Main
